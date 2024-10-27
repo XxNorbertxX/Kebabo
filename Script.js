378a8bbe2d67c7a8558bbdcf9442815459d5fae4
XmastiX <mail@gmail.com>
2024-10-27 22:06:21 +0100

    dodane menu po scrolowaniu

--- Script.js
+++ Script.js
@@ -4,3 +4,25 @@ $('section nav ul li a').on('click', function() {
         scrollTop: $(goToSection).offset().top
     })
 })
+
+const burger = document.querySelector(".burger");
+
+const iconBurger = document.querySelector(".fa-bars");
+const iconX = document.querySelector(".fa-xmark");
+const column = document.querySelector("ul");
+
+burger.addEventListener("click", function() {
+    iconBurger.classList.toggle("show");
+    iconX.classList.toggle("show");
+    column.classList.toggle("show");
+})
+
+window.addEventListener('scroll', function() {
+    const menu = this.document.getElementById("Burger");
+    if (this.window.scrollY > 140) {
+        menu.classList.add("show");
+    } else {
+        menu.classList.remove("show");
+    }
+})
+ 
