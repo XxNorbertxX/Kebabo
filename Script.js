4d44657e57efe0707e17f68b7be167c271c3788c
Pablo <xscorpionx@o2.pl>
2025-01-11 17:25:07 +0100

    -  wersja robocza nowej koncepcji menu jedzienia

--- Script.js
+++ Script.js
@@ -121,6 +121,6 @@ $(document).on('scroll', function() {
         $bread.addClass('flow');        
     }
     if (scrollValue < 100) {
-        $('.foodMenu .foodMenuItems div').removeClass('flow');    
+        $('.foodMenu div').removeClass('flow');    
     }
 })
